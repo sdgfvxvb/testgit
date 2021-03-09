478668b9d8eda47dc39c959989975cca154c51fa
396220003@qq.com <396220003@qq.com>
2021-03-09 22:35:14 +0800

    rebase b

--- app/src/main/java/com/example/testgit/MainActivity.java
+++ app/src/main/java/com/example/testgit/MainActivity.java
@@ -10,7 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        System.out.println("d");
+        System.out.println("a");
+        System.out.println("b");
+        System.out.println("c");
     }
 }
