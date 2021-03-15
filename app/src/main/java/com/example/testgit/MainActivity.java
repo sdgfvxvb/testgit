ecc27d63598c4c8e76db24d7705c28fca0546179
396220003@qq.com <396220003@qq.com>
2021-03-15 21:50:02 +0800

    sit a

--- app/src/main/java/com/example/testgit/MainActivity.java
+++ app/src/main/java/com/example/testgit/MainActivity.java
@@ -11,6 +11,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        System.out.println("sit a");
+
         System.out.println("uat a");
         System.out.println("uat c");
     }
